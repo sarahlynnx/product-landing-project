e9cb8f56a78811d8b427491f947dc34f0ca2140d
sarahlynnx <xsarahlynn06x@gmail.com>
2024-03-06 19:41:05 -0800

    Initial development

---
+++ index.js
@@ -0,0 +1,13 @@
+const flavors = [
+    'Dark Chocolate + Sea Salt',
+    'Dark Chocolate + Blueberries',
+    'Dark Chocolate + Cherries',
+    'Dark Chocolate + Pumpkin Spice',
+    'Dark Chocolate + Pomegranate',
+    'Milk Chocolate',
+    'Milk Chocolate + Cherries',
+    'Milk Chocolate + Currants',
+    'Milk Chocolate Sampler',
+    'Dark Chocolate Sampler',
+    'Variety Sampler',
+]
